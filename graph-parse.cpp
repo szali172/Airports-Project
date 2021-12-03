ec0d01c21e3df93db95722caaa8ae0e7a6931710
Daniel Rugutt <drugutt2@illinois.edu>
2021-12-03 19:24:34 +0000

    recommit Graph constructor

--- graph-parse.cpp
+++ graph-parse.cpp
@@ -1,7 +1,31 @@
 #include "graph.h"
 
 Graph::Graph(std::string airports_file, std::string routes_file) {
-    for (int i = 0; i < 14,110; i++) {
-        int x = 1;
+
+    std::ifstream data(airports_file);
+    std::string line;
+
+    double longtitude;
+    double latitude;
+
+    const size_t LONGITUDE_INDEX = 6;
+    const size_t LATITUDE_INDEX = 7;
+    int fileIndex = 0;
+
+    while (std::getline(data, line)) {
+        std::stringstream lineStream(line);
+        std::string aCell;
+        fileIndex = 0;
+
+        while (std::getline(lineStream, aCell, ',')) {
+            if (fileIndex == LONGITUDE_INDEX) {
+                longtitude = std::stod(aCell);
+            } else if (fileIndex == LATITUDE_INDEX) {
+                latitude = std::stod(aCell);
+            }
+            ++fileIndex;
+        }
+
+        adjacency_list->push_back(new Edge{std::pair<double, double>(longtitude, latitude), nullptr});
     }
-}
+} 
