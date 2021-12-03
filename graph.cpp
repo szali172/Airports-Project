d09aae134bc8a279389c2b49b30caf202a561e00
Priya Kumar <priyak5@illinois.edu>
2021-12-03 01:58:33 -0600

    wrote dijkstra function

--- graph.cpp
+++ graph.cpp
@@ -3,7 +3,7 @@
 /* 
 / Dijkstra's algorithm to find the shortest path for a graph using adjacency list
 */
-Graph* dijkstra(Graph graph, int start) {
+Graph* dijkstra(Graph *graph, int start) {
     for (Vertex v : graph) {
         dist[v] = +inf;
         prev[v] = NULL;
@@ -20,6 +20,15 @@ Graph* dijkstra(Graph graph, int start) {
     }
 
     while(!Q.empty()) {
-        
+        Vertex u = Q.removeMin();
+        T.add(u);
+        for (list<node>::iterator iter = graph.adjList[u].begin(); iter != graph.adjList[u].end(); iter++) {
+            if ((dist[u] + iter->cost) < dist[iter->dest]) {
+                dist[iter->dest] = dist[u] + iter->cost;
+                prev[iter->dest] = u;
+            }
+        }
     }
+
+    return T;
 }
