4527059206519998a54051eca5a5ad5aa3d011f7
Zuhair Ali <sali200@illinois.edu>
2021-12-11 02:42:23 -0600

    pre-refactor for cleanliness

--- graph.h
+++ graph.h
@@ -117,7 +117,7 @@ class Graph {
     * @param starting node
     * @return SSSP graph
     */
-    Graph* dijkstra(Graph* graph, int start);
+    Graph dijkstra(Graph* graph, int start);
 
     double calculateDistance(double longitude1, double latitude1, double longitude2, double latitude2);
 
---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include "graph.h"
+
+#include <iostream>
+#include <string>
+
+int main() {
+    // // create graph/adjacency list with all airports and all connections
+    // Graph* graph = new Graph("airports.csv", "routes.dat");
+    // // run dijkstra's on it, include source airport
+    // int source = graph->locateStart("airports.csv", "ORD");
+    // graph->dijkstra(graph, source);
+    // // print output (new adjacency list representing SSSP) to map PNG
+
+    std::cout << "hi" << std::endl;
+
+    return 0;
+}
